799d40800358e88f87ac2d2b8bddc7d655fd97eb
iMnX <imnx.au@outlook.com>
2017-10-12 22:21:21 +1100

    updated some shit.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sudo /bin/bash
-echo "set base_path         /var/spool/apt-mirror
+echo "set base_path         /mnt/sdb/apt-mirror
 set mirror_path       $base_path/mirror
 set skel_path         $base_path/skel
 set var_path          $base_path/var
 set postmirror_script $var_path/postmirror.sh
-set defaultarch       i386
+set defaultarch       armhf
 set run_postmirror    0
 set nthreads          20
 set limit_rate        100m
@@ -18,14 +18,10 @@ set http_proxy        127.0.0.1:3128
 set proxy_user        user
 set proxy_password    password
 
-deb http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty main restricted universe multiverse
-deb http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-security main restricted universe multiverse
-deb http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-updates main restricted universe multiverse
-deb http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-backports main restricted universe multiverse
+deb http://mirror.internode.on.net/pub/raspbian/raspbian jessie main contrib non-free firmware rpi
+deb http://mirror.internode.on.net/pub/raspbian/raspbian jessie-staging main contrib non-free firmware rpi
 
-#deb-src http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty main restricted universe multiverse
-#deb-src http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-security main restricted universe multiverse
-#deb-src http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-updates main restricted universe multiverse
-#deb-src http://mirror.internode.on.net/pub/ubuntu/ubuntu zesty-backports main restricted universe multiverse
+#deb-src http://mirror.internode.on.net/pub/raspbian/raspbian jessie main contrib non-free firmware rpi
+#deb-src http://mirror.internode.on.net/pub/raspbian/raspbian jessie-staging main contrib non-free firmware rpi
 
-clean http://mirror.internode.on.net/pub/ubuntu/ubuntu" > mirror.list
+clean http://mirror.internode.on.net/pub/raspbian/raspbian" > mirror.list
